6bd4c13df0dde3beed22563d4f126120787e8d78
Kevin Chau <kchau57@uwo.ca>
2023-10-24 20:57:14 -0400

    third commit

--- news-app/src/app/page.tsx
+++ news-app/src/app/page.tsx
@@ -1,15 +1,24 @@
-import { categories } from "../../constants";
 import { fetchNews } from "../../lib/fetchNews";
+import NewsArticle from "./NewsArticle"; // Import the NewsArticle component
+import { useRouter } from "next/router";
 
 
 async function Home() {
-  const news = await fetchNews(categories.join(','))
-  console.log(news);
+  const category: Category = "Technology";
+  const news = await fetchNews(category);
+
+  // Extract the first three articles
+  const firstThreeArticles = news.data.slice(0, 6);
+
   return (
     <div>
-      {/* News List */}
+      {/* Map through the first three articles and render them using the NewsArticle component */}
+      {firstThreeArticles.map((article: DataEntry, index: number) => (
+        <NewsArticle key={index} article={article} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
